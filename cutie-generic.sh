acf8cd215479c9bcb8e82b412667090c9cf99ead
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2025-07-09 20:58:47 +0530

    include     options for  qml-module-cutiewlc        cutie-wlc

--- cutie-generic.sh
+++ cutie-generic.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 echo "Welcome to Cutie Generic Project Builder
-       Please input a project name to build
-       Available projects are:
-        1: cutie-home
-        2: cutie-launcher
-        3: libcutiedesktopfilephraser"
+        Please input a project name to build
+        Available projects are:
+         1: cutie-home
+         2: cutie-launcher
+         3: libcutiedesktopfilephraser
+         4: qml-module-cutiewlc
+         5: cutie-wlc"
         
 read -p "Please input project name: " input
 
@@ -16,6 +18,10 @@ elif [[ "$input" == "cutie-launcher" || "$input" == "2" ]]; then
   project="cutie-launcher"
 elif [[ "$input" == "libcutiedesktopfilephraser" || "$input" == "3" ]]; then
   project="libcutiedesktopfilephraser"
+elif [[ "$input" == "qml-module-cutiewlc" || "$input" == "4" ]]; then
+  project="qml-module-cutiewlc"
+elif [[ "$input" == "cutie-wlc" || "$input" == "5" ]]; then
+  project="cutie-wlc"
 else
   echo "Invalid input. Please try again." && exit 1
 fi
@@ -24,9 +30,9 @@ echo "Building $project ..."
 sleep .5
 
 if [ -d "$project" ] ;then
- echo "Directory $project exists. Pulling latest changes..."
- cd $project
- git pull
+  echo "Directory $project exists. Pulling latest changes..."
+  cd $project
+  git pull
 
 else
   echo "Cloning $project repository..."
@@ -48,4 +54,3 @@ make || { echo "Make failed."; exit 1; }
 sudo make install || { echo "Installation failed."; exit 1; }
 
 echo "$project has been successfully built and installed!"
-
